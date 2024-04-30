a167b3b4c95408d4f2be60fb3f30fba62115ea18
AniketDange23 <aniketdange2321@gmail.com>
2024-04-30 13:11:30 +0530

    initial

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -9,14 +9,14 @@ import { Link } from "react-router-dom";
 
 const TopBar = () => {
   return (
-    <div className='container-fluid bg-secondary g-0 align-content-center '>
+    <div className='container-fluid bg-secondary g-0 contact align-content-center '>
       <div className='row ' style={{ margin: "12px" }}>
         {/* social icons */}
-        <div className='col-lg-5 '>
+        <div className='col-lg-5  '>
           <span className='text-white'>Connect Us:</span>
           <ul className='list-inline d-inline-block'>
             <li className='list-inline-item'>
-              <Link className='text-white d-inline-block p-2' to='#'>
+              <Link className='text-white d-inline-block' to='#'>
                 <FaFacebookF style={{ fontSize: "16px" }} />
               </Link>
             </li>
@@ -49,7 +49,7 @@ const TopBar = () => {
                 className='text-white d-inline-block px-lg-3 px-2'
                 to='callto:+91 8830826434'
               >
-                <MdPhoneInTalk /> Call Us Now :{" "}
+                <MdPhoneInTalk /> Call Now :{" "}
                 <span className=''>+91 8830826434</span>
               </Link>
             </li>
