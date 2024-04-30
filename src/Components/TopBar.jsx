c5dd213943acbca929e7301c91242a86c1c73a2c
AniketDange23 <aniketdange2321@gmail.com>
2024-04-30 14:29:28 +0530

    completed

--- src/Components/TopBar.jsx
+++ src/Components/TopBar.jsx
@@ -6,54 +6,55 @@ import { IoMail } from "react-icons/io5";
 import { MdPhoneInTalk } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-
 const TopBar = () => {
   return (
-    <div className='container-fluid bg-secondary g-0 contact align-content-center '>
-      <div className='row ' style={{ margin: "12px" }}>
-        {/* social icons */}
-        <div className='col-lg-5  '>
-          <span className='text-white'>Connect Us:</span>
-          <ul className='list-inline d-inline-block'>
-            <li className='list-inline-item'>
-              <Link className='text-white d-inline-block' to='#'>
-                <FaFacebookF style={{ fontSize: "16px" }} />
-              </Link>
-            </li>
-            <li className='list-inline-item'>
-              <Link className='text-white d-inline-block p-2' to='#'>
-                <FaInstagram style={{ fontSize: "16px" }} />
-              </Link>
-            </li>
-            <li className='list-inline-item'>
-              <Link className='text-white d-inline-block p-2' to='#'>
-                <FaLinkedin style={{ fontSize: "16px" }} />
-              </Link>
-            </li>
-          </ul>
-        </div>
-        {/* mail and phone */}
-        <div className='col m-lg-2 d-flex  text-center justify-content-end'>
-          <ul className='list-inline'>
-            <li className='list-inline-item mx-0 border-right border-secondary'>
-              <Link
-                className='text-white d-inline-block px-lg-3 px-2'
-                to='mailto:sales@midlinesmedia.com'
-              >
-                {" "}
-                <IoMail /> sales@midlinesmedia.com
-              </Link>
-            </li>
-            <li className='list-inline-item mx-0'>
-              <Link
-                className='text-white d-inline-block px-lg-3 px-2'
-                to='callto:+91 8830826434'
-              >
-                <MdPhoneInTalk /> Call Now :{" "}
-                <span className=''>+91 8830826434</span>
-              </Link>
-            </li>
-          </ul>
+    <div className='top-header bg-secondary  '>
+      <div className='container-fluid px-4 py-1'>
+        <div className='row align-items-center'>
+          {/* social icons */}
+          <div className='col-md-6 text-center text-md-start'>
+            <span className='text-white'>Connect Us -</span>
+            <ul className='list-inline d-inline-block'>
+              <li className='list-inline-item'>
+                <Link className='text-white d-inline-block p-2' to='#'>
+                  <FaFacebookF style={{ fontSize: "16px" }} />
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link className='text-white d-inline-block p-2' to='#'>
+                  <FaInstagram style={{ fontSize: "16px" }} />
+                </Link>
+              </li>
+              <li className='list-inline-item'>
+                <Link className='text-white d-inline-block p-2' to='#'>
+                  <FaLinkedin style={{ fontSize: "16px" }} />
+                </Link>
+              </li>
+            </ul>
+          </div>
+          {/* mail and phone */}
+          <div className='col-md-6 text-center   text-sm-end'>
+            <ul className='list-inline'>
+              <li className='list-inline-item mx-0 border-right border-secondary'>
+                <Link
+                  className='text-white d-inline-block px-lg-3 px-2'
+                  to='mailto:sales@midlinesmedia.com'
+                >
+                  {" "}
+                  <IoMail /> sales@midlinesmedia.com
+                </Link>
+              </li>
+              <li className='list-inline-item mx-0'>
+                <Link
+                  className='text-white d-inline-block px-lg-3 px-2'
+                  to='callto:+91 8830826434'
+                >
+                  <MdPhoneInTalk /> Call Now :{" "}
+                  <span className=''>+91 8830826434</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
