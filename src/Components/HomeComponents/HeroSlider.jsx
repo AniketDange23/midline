6edd9602bf286bc449eb426335c9602754beecb4
AniketDange23 <aniketdange2321@gmail.com>
2024-05-06 17:57:39 +0530

    complete

--- src/Components/HomeComponents/HeroSlider.jsx
+++ src/Components/HomeComponents/HeroSlider.jsx
@@ -1,42 +1,19 @@
-
-
 const HeroSlider = () => {
-
   return (
     <>
-
- 
-    {/* <div className="video-wrapper">
-      <video
-        autoPlay
-        muted
-        loop
-        playsInline
-        preload="metadata"
-        controls={false}
-        className="img-fluid"
-        style={{ width: "100%"}}
-      >
-        <source src="/video.mp4" type="video/mp4" />
-      </video>
-    </div> */}
-    <div>
-<div className="container   ">
-  <div className="row ">
-  <div className="col-lg-6 align-content-center ">
-    <h1>Discover Our Expertise in Call Center Services
-</h1>
-    <h4 className="py-2">we specialize in providing unparalleled call center management services across Asia and America.</h4>
-
-    <p className="py-3">Midline Media Solutions remains steadfast in its commitment to excellence, continuously evolving to stay ahead of the curve and provide unparalleled service that empowers our clients to achieve their business goals.</p>
-  </div>
-  <div className="col-lg-6 py-5  align-content-center justify-content-center text-center">
-   <img src="/image2.jpg" className="img-fluid " alt="" />
-  </div>
-</div>
-</div>
-</div>
-   </> 
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 align-content-center">
+            <h1>Discover Our Expertise in Call Center Services</h1>
+            <h4 className="py-2">We specialize in providing unparalleled call center management services across Asia and America.</h4>
+            <p className="py-3">Midline Media Solutions remains steadfast in its commitment to excellence, continuously evolving to stay ahead of the curve and provide unparalleled service that empowers our clients to achieve their business goals.</p>
+          </div>
+          <div className="col-lg-6 py-5 align-content-center justify-content-center text-center">
+            <img src="/image2.jpg" className="img-fluid" alt="" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
