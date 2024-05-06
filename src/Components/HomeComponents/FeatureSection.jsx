28279e4def485d0e336f776e3ae4d4f333dab9b5
AniketDange23 <aniketdange2321@gmail.com>
2024-05-06 17:43:35 +0530

    completed

--- src/Components/HomeComponents/FeatureSection.jsx
+++ src/Components/HomeComponents/FeatureSection.jsx
@@ -3,84 +3,63 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { Link } from "react-router-dom";
 
-const data = [
-  {
-    count: 3,
-    title: " Years Experience",
-    description:
-      "Three years of dedicated innovation, delivering excellence every step of the way.",
-  },
-  {
-    count: 9,
-    title: "Happy Clients",
-    description:
-      "Serving 9 satisfied clients with exceptional service and results that exceed expectations.",
-  },
-  {
-    count: 250,
-    title: " Best Calling Service",
-    description:
-      "Providing the best calling service to 250 clients, ensuring seamless communication.",
-  },
-  {
-    count: 70,
-    title: "Expert Workers",
-    description:
-      "Our team of 70 experts brings unparalleled skills and knowledge to every project.",
-  },
-];
+// const data = [
+//   {
+//     count: 3,
+//     title: " Years Experience",
+//     description:
+//       "Three years of dedicated innovation, delivering excellence every step of the way.",
+//   },
+//   {
+//     count: 9,
+//     title: "Happy Clients",
+//     description:
+//       "Serving 9 satisfied clients with exceptional service and results that exceed expectations.",
+//   },
+//   {
+//     count: 250,
+//     title: " Best Calling Service",
+//     description:
+//       "Providing the best calling service to 250 clients, ensuring seamless communication.",
+//   },
+//   {
+//     count: 70,
+//     title: "Expert Workers",
+//     description:
+//       "Our team of 70 experts brings unparalleled skills and knowledge to every project.",
+//   },
+// ];
 
 const FeatureSection = () => {
-  const [counts, setCounts] = useState(Array(data.length).fill(0));
-
-  useEffect(() => {
-    AOS.init();
-    // Initialize counts animation
-    const interval = setInterval(() => {
-      setCounts((prevCounts) =>
-        prevCounts.map((count, index) =>
-          count < data[index].count ? count + 1 : count
-        )
-      );
-    }, 50); // Adjust the interval for smoother animation or performance
-    return () => clearInterval(interval);
-  }, []);
-
   return (
-    <div className="container-fluid">
-      <div className="row align-items-center justify-content-center">
-        <div className="col-lg-5 py-3 px-3">
-          <h3 className="mb-3 position-relative">Welcome to MIDLINE Media Solution</h3>
+    <div className='container '>
+      <div className='row    py-5'>
+        <div className='col-lg-6 col-md-8   justify-content-center d-flex'>
+          <img
+            src='./image1-min.JPG'
+            className='img-fluid rounded-2 shadow'
+            style={{ width: "550px", objectFit:"cover" }}
+            alt=''
+          />
+        </div>
+        <div className='col-lg-6 py-5'>
+          <h3 className='mb-3 position-relative'>
+            Welcome to MIDLINE Media Solution
+          </h3>
           <p>
-            Midline media solution founded in 2020. Over the years, we have
-            grown into a versatile and forward-thinking organization, dedicated
-            to empowering businesses across various industries. Our team
-            comprises talented professionals from diverse backgrounds, allowing
-            us to offer comprehensive solutions designed to meet the unique
-            needs of our clients.
+            Midline Media Solutions, founded in 2020, has rapidly evolved into a
+            versatile and forward-thinking organization dedicated to empowering
+            businesses across various industries. Our team comprises talented
+            professionals from diverse backgrounds, enabling us to offer
+            comprehensive solutions designed to meet the unique needs of our
+            clients. This commitment to delivering customized solutions has
+            fueled our growth and solidified our position as a leading call
+            center in Nagpur.
           </p>
           <br />
-          <button className="button">
-            <Link to="/services">Know More</Link>
+          <button className='button'>
+            <Link to='/services'>Know More</Link>
           </button>
-        </div>
-        <div className="col-lg-6 col-md-8 py-">
-          <div className="row">
-            {data.map((item, index) => (
-              <div className="col-lg-6 p-3" key={index}>
-                <div className="card">
-                  <h5
-                    className="icon-bg text-white water-wave text-center py-4 count"
-                    data-count={item.count}
-                  >
-                    {counts[index]}
-                  </h5>
-                  <h4>{item.title}</h4>
-                  <span>{item.description}</span>
-                </div>
-              </div>
-            ))}
-          </div>
         </div>
       </div>
     </div>
--- src/Components/HomeComponents/HeroSlider.jsx
+++ src/Components/HomeComponents/HeroSlider.jsx
@@ -3,8 +3,10 @@
 const HeroSlider = () => {
 
   return (
-    <section className="hero-slider">
-    <div className="video-wrapper">
+    <>
+
+ 
+    {/* <div className="video-wrapper">
       <video
         autoPlay
         muted
@@ -17,9 +19,24 @@ const HeroSlider = () => {
       >
         <source src="/video.mp4" type="video/mp4" />
       </video>
-    </div>
+    </div> */}
+    <div>
+<div className="container   ">
+  <div className="row ">
+  <div className="col-lg-6 align-content-center ">
+    <h1>Discover Our Expertise in Call Center Services
+</h1>
+    <h4 className="py-2">we specialize in providing unparalleled call center management services across Asia and America.</h4>
 
-    </section>
+    <p className="py-3">Midline Media Solutions remains steadfast in its commitment to excellence, continuously evolving to stay ahead of the curve and provide unparalleled service that empowers our clients to achieve their business goals.</p>
+  </div>
+  <div className="col-lg-6 py-5  align-content-center justify-content-center text-center">
+   <img src="/image2.jpg" className="img-fluid " alt="" />
+  </div>
+</div>
+</div>
+</div>
+   </> 
   );
 };
 
