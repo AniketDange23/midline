0f51e0cd71f78af77e4a9c17a056e5cbfbe72ffb
AniketDange23 <aniketdange2321@gmail.com>
2024-05-26 21:21:38 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import TopBar from './Components/TopBar';
 import BackToTopButton from './Components/FixedButtons';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import "aos/dist/aos.css"; // Import AOS CSS
+import { useEffect } from 'react';
 
 const App = () => {
- 
+  const location = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]); // Scroll to top whenever the pathname changes
+
   return (
     <div>
      
